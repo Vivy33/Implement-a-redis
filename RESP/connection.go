dda0fae9dcba5fdd7beacde0ad2fe332a2128270
vivy33 <vivymomo0407@gmail.com>
2025-01-26 16:38:22 +0800

    up comment

--- RESP/connection.go
+++ RESP/connection.go
@@ -6,12 +6,15 @@ import (
 )
 
 // 表示一个 Redis 连接
+// 用于管理与客户端的网络连接
+// 它封装了底层的网络连接和用于高效读写的缓冲器
 type Connection struct {
     conn   net.Conn
     reader *bufio.Reader
     writer *bufio.Writer
 }
 
+// 它接受一个 net.Conn 作为参数，并初始化相应的读写缓冲器
 func NewConnection(conn net.Conn) *Connection {
     return &Connection{
         conn:   conn,
@@ -21,11 +24,13 @@ func NewConnection(conn net.Conn) *Connection {
 }
 
 // 从连接中读取一个 RESP 值
+// 如果读取过程中发生错误，它会返回该错误
 func (c *Connection) Read() (RESPValue, error) {
     return ParseRESP(c.reader)
 }
 
 // 将一个 RESP 值写入连接
+// 如果读取过程中发生错误，它会返回该错误
 func (c *Connection) Write(reply Reply) error {
     _, err := reply.WriteTo(c.writer)
     if err != nil {
@@ -35,6 +40,7 @@ func (c *Connection) Write(reply Reply) error {
 }
 
 // 关闭连接
+// 它返回关闭过程中可能发生的任何错误
 func (c *Connection) Close() error {
     return c.conn.Close()
 }
--- RESP/parsestream.go
+++ RESP/parsestream.go
@@ -5,6 +5,7 @@ import (
     "io"
 )
 
+// 提供了一个流式接口来解析 RESP 数据
 type ParseStream struct {
     reader *bufio.Reader
 }
@@ -13,6 +14,7 @@ func NewParseStream(r io.Reader) *ParseStream {
     return &ParseStream{reader: bufio.NewReader(r)}
 }
 
+// 从流中读取并解析下一个 RESP 值
 func (ps *ParseStream) Next() (RESPValue, error) {
     return ParseRESP(ps.reader)
 }
